17dcbadfbabe78c25df40ff05967f8e2bb9c5f37
Julio César <jcesarmobile@gmail.com>
2018-05-16 20:55:36 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage, NavParams } from 'ionic-angular';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { CameraSource, CameraResultType, Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  image: SafeResourceUrl;
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    private sanitizer: DomSanitizer) {
   }
 
+  takePicture(): void {
+    take();
+  }
+
+  
+
+}
+
+async function take() {
+  const { Camera } = Plugins;
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Base64,
+        source: CameraSource.Photos
+    });
+
+    this.image = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.base64Data));
+
+    console.log(this.image);
 }
